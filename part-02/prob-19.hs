412e3efb44eace4f8865d0656fc617d99161da5a
kalaluthien <kalaluthien@gmail.com>
2021-05-21 00:05:56 +0900

    Solve prob 19

--- part-02/prob-19.hs
+++ part-02/prob-19.hs
@@ -1,1 +1,7 @@
 rotate :: [a] -> Int -> [a]
+rotate [] _ = []
+rotate l 0 = l
+rotate l n
+  | n > 0 = rotate (tail l ++ [head l]) (n - 1)
+  | n < 0 = rotate (last l : init l) (n + 1)
+  | otherwise = l
