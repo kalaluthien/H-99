02388da08663dc84ff49b1f437ff2308fe2f0678
kalaluthien <kalaluthien@gmail.com>
2021-05-20 23:19:04 +0900

    Solved problem 17

--- part-02/prob-17.hs
+++ part-02/prob-17.hs
@@ -1,1 +1,6 @@
 split :: [a] -> Int -> ([a], [a])
+split l n = go n [] l
+    where go :: Int -> [a] -> [a] -> ([a], [a])
+          go _ xs [] = (xs, [])
+          go 0 xs ys = (xs, ys)
+          go n xs (y:ys) = go (n - 1) (xs ++ [y]) ys
