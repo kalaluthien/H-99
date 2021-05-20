2754b1f972d3901ef466ceece981466756424a6c
kalaluthien <kalaluthien@gmail.com>
2021-05-20 22:24:33 +0900

    Solve prob 12

--- part-02/prob-12.hs
+++ part-02/prob-12.hs
@@ -2,3 +2,6 @@ data ListItem a = Single a | Multiple Int a
     deriving (Show)
 
 decodeModified :: Eq a => [ListItem a] -> [a]
+decodeModified = (>>= go)
+    where go (Single v) = [v]
+          go (Multiple n v) = take n $ repeat v
