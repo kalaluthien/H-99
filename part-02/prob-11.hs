0357ddaa286faf5b719742816edfdc813ed4a7ee
kalaluthien <kalaluthien@gmail.com>
2021-05-20 22:34:09 +0900

    Rewrite prob 11 & solve prob 12

--- part-02/prob-11.hs
+++ part-02/prob-11.hs
@@ -2,12 +2,14 @@ data ListItem a = Single a | Multiple Int a
     deriving (Show)
 
 encodeModified :: Eq a => [a] -> [ListItem a]
-encodeModified = foldr encode1 []
-    where encode1 :: Eq a => a -> [ListItem a] -> [ListItem a]
-          encode1 x [] = [Single x]
-          encode1 x ((Single y):ys)
-            | x == y = Multiple 2 x : ys
-            | otherwise = Single x : ((Single y):ys)
-          encode1 x ((Multiple n y):ys)
-            | x == y = Multiple (n + 1) y : ys
-            | otherwise = Single x : ((Multiple n y):ys)
+encodeModified l = [transform x | x <- encode l]
+    where transform :: Eq a => (Int, a) -> ListItem a
+          transform (1, x) = Single x
+          transform (n, x) = Multiple n x
+          encode :: Eq a => [a] -> [(Int, a)]
+          encode = foldr encode1 []
+          encode1 :: Eq a => a -> [(Int, a)] -> [(Int, a)]
+          encode1 x [] = [(1, x)]
+          encode1 x (y:ys)
+            | x == snd y = (fst y + 1, x) : ys
+            | otherwise  = (1, x) : (y:ys)
